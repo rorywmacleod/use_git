d351bdb1a76d07f78f42620aefd9bdfb37908b0b
Rory MacLeod <rory.macleod@btinternet.com>
2019-09-04 09:56:41 +0100

    Version 1.1 with __str__

--- test_thing.py
+++ test_thing.py
@@ -3,3 +3,6 @@ from thing import Thing
 def test_value():
     t = Thing(42)
     assert t.Value == 42
+def test_str():
+    t = Thing(42)
+    assert str(t) == "Thing: 42"
--- thing.py
+++ thing.py
@@ -1,9 +1,11 @@
 class Thing:
     '''
-    v 1.0 
+    v 1.1 added __str__
     '''
     def __init__(self, value):
         self.__value = value
     @property
     def Value(self):
         return self.__value
+    def __str__(self):
+        return f'Thing: {self.Value}'
